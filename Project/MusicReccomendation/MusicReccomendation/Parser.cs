574854a306bb8300e1649a3757e55658edae4184
tomerwi <tomerwi@post.bgu.ac.il>
2016-06-14 15:07:40 +0300

    update

---
+++ Project/MusicReccomendation/MusicReccomendation/Parser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MusicReccomendation
+{
+    
+    class Parser
+    {
+
+        XmlReader reader;
+        string allText;
+
+        public Parser(string xmlPath)
+        {
+            readXmlAsString(xmlPath);
+           
+        }
+
+        
+
+        public void readXmlAsString(string xmlPath)
+        {
+            allText = System.IO.File.ReadAllText(@xmlPath);
+            reader = XmlReader.Create(allText);
+
+        }
+
+        public void parseXML()
+        {
+            reader.ReadToFollowing("Entry");
+        }
+
+
+            
+    }
+}
---
+++ Project/MusicReccomendation/MusicReccomendation/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicReccomendation
+{
+    class Song
+    {
+        public enum Key { };
+        private string id;
+        private float bpm;
+        private string title;
+        private string artist;
+        private List<string> genre;
+        private int playCount;
+        private Key key;
+
+
+    }
+}
