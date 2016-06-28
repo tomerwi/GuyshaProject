7c8be92435c97e896179c4efa7737ffce4fa6a1d
tomerwi <tomerwi@post.bgu.ac.il>
2016-06-28 15:46:45 +0300

    Pushpushon

--- Project/MusicReccomendation/MusicReccomendation/RecommendationSystem.cs
+++ Project/MusicReccomendation/MusicReccomendation/RecommendationSystem.cs
@@ -25,20 +25,19 @@ namespace MusicReccomendation
 
         public Song Recommend(List<Song> playList)
         {
-            //playcount
+            
             if (DataSet == null || DataSet.Count == 0)
                 return null;
             Random rnd = new Random();
-            int limit = 1000;
-            //List<Song> zero = DataSet.Where(x => x.playCount == 0).ToList();
-            List<Song> randomData = DataSet.Where(x => x.playCount > 2).ToList();
+            int limit = 1000; 
+            List<Song> randomData = DataSet.Where(x => x.playCount > 2).ToList(); //taking only the songs which were played more then twice
             if (randomData.Count < limit)
                 limit = randomData.Count;
-            randomData = randomData.OrderByDescending(x => (x.playCount * rnd.Next())).Take(limit).Where(x=> !playList.Contains(x)).ToList();
+            randomData = randomData.OrderByDescending(x => (x.playCount * rnd.Next())).Take(limit).Where(x=> !playList.Contains(x)).ToList(); //dont take the songs that were already played in the current playlist
             Song ans = randomData.First();
             if (playList == null || playList.Count == 0)
             {
-                return ans;
+                return ans; //choose random song to begin the playlist with
             }
             double maxScore = calcScore(ans,playList); 
             randomData.ForEach(song =>
@@ -62,17 +61,20 @@ namespace MusicReccomendation
             {
                 index++;
                 double sim = calcSimilarity(song, psong);
-                score += (index * sim);
+                score += (index * sim); //give more weight to songs that was recenlty played
             });
-            score = score / playList.Count;
+            score = score / playList.Count; 
             return score;
         }
 
         private double calcSimilarity(Song song1, Song song2)
         {
+            //BPM Similarity (Maximun value - 1 )
             double bpmSim = 1 - (Math.Abs(song1.bpm - song2.bpm)/ 60 ) ;
             if (bpmSim < 0)
                 bpmSim = 0;
+
+            //Key Similarity (Maximum value - 1)
             double keySim = 0;
             if (song1.key.Equals(song2.key))
                 keySim = 1;
@@ -87,9 +89,13 @@ namespace MusicReccomendation
                 int maxLength = Math.Max(song1.key.Length, song2.key.Length);
                 keySim = countSimKey / maxLength;
             }
+
+            //Artist Similarity
             double artistSim = 0;
             if (song1.artist.Equals(song2.artist))
                 artistSim = 0.5;
+
+            //Genre Similarity
             int countSimGenre = 0;
             if(song1.genre.Count !=0 && song2.genre.Count != 0)
             {
